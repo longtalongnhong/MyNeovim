98c6c224b580c670355a4889cd465a04e5641f3b
longtalongnhong <longtalongnhong@gmail.com>
2021-12-18 08:13:20 +0700

    colorscheme

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -9,31 +9,38 @@ local fn = vim.fn
 -- Install packer automatically
 local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
 if fn.empty(fn.glob(install_path)) > 0 then
-  PACKER_BOOTSTRAP = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-  print("Packer is installing ...")
+  PACKER_BOOTSTRAP = fn.system {
+    "git",
+    "clone",
+    "--depth",
+    "1",
+    "https://github.com/wbthomason/packer.nvim",
+    install_path,
+  }
+  -- This will install packer and close nvim
+  vim.cmd [[ packadd packer.nvim ]]
 end
 
 -- Auto load plugins file whenever save the file
-vim.cmd([[
+vim.cmd[[
   augroup packer_user_config
     autocmd!
     autocmd BufWritePost plugins.lua source <afile> | PackerSync
   augroup end
-]])
+]]
 
 local status_ok, packer = pcall(require, "packer")
 if not status_ok then
   return
 end
 
-packer.init({
+packer.init{
   display = {
     open_fn = function()
-      return require('packer.util').float({ border = 'single'})
-    end
-    }
+      return require('packer.util').float({ border = 'rounded'})
+    end,
+    },
   }
-)
 
 return packer.startup(function(use)
   use "wbthomason/packer.nvim"  --Packer has manage it self
@@ -48,6 +55,7 @@ return packer.startup(function(use)
     },
     config = function() require'nvim-tree'.setup {} end
 	}															-- Files management
+  use "lunarvim/darkplus.nvim"
 
   -- Automatically set up your configuration after cloning packer.nvim
   if  PACKER_BOOTSTRAP then
